ec867ce4bfe0f172b4b6061172a7b9794d3a1320
1iken <tavorearljohn@gmail.com>
2022-07-27 20:07:14 +0800

    initialize basic express app

---
+++ server.js
@@ -0,0 +1,18 @@
+//server dependancies
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+app.use(express.urlencoded({extended:true}));
+
+//connection string
+mongoose.connect(process.env.MONGODB_URI).then(console.log('Connected')).catch(err => console.log(err));
+
+
+app.get("/", (req,res)=>{
+    res.status(200).send('Hello world!')
+})
+app.listen(3000)
