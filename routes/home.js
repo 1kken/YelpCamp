959cf249c21346fe014f7e7c1e1c70d5a6b8e48f
1iken <tavorearljohn@gmail.com>
2022-07-28 21:33:27 +0800

    added individual view file

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,21 @@
-const { Router } = require('express');
-const express = require('express');
+const { Router } = require("express");
+const express = require("express");
 const home = express.Router();
-const campGround = require('../models/campground');
+const campGround = require("../models/campground");
 
-home.get('/',async (req,res)=>{
-    const datas = await campGround.find({}); 
-    res.render('campground/index',{datas})   
-})
+home.get("/", async (req, res) => {
+  const datas = await campGround.find({});
+  res.render("campground/index", { datas });
+});
 
+home.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const datas = await campGround.findById(id);
+  res.render("campground/show", { datas });
+});
 
+home.get("/createNew", async (req, res) => {
+  res.send("hello!");
+});
+console.log("hello world!");
 module.exports = home;
