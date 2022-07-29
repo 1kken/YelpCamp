a33755914b00bbcd6940af6ba31efb461574209c
1iken <tavorearljohn@gmail.com>
2022-07-29 16:58:48 +0800

    add functionalities

--- routes/home.js
+++ routes/home.js
@@ -1,21 +1,68 @@
 const { Router } = require("express");
 const express = require("express");
+const { findById } = require("../models/campground");
 const home = express.Router();
 const campGround = require("../models/campground");
+const methodOverride = require('method-override')
 
+//show all campGrounds
 home.get("/", async (req, res) => {
   const datas = await campGround.find({});
   res.render("campground/index", { datas });
 });
 
+//Show form page
+home.get("/new", (req, res) => {
+  res.render("campground/createCamp");
+});
+
+//show individual campground
 home.get("/:id", async (req, res) => {
   const id = req.params.id;
   const datas = await campGround.findById(id);
   res.render("campground/show", { datas });
 });
 
-home.get("/createNew", async (req, res) => {
-  res.send("hello!");
+//Add new Camp ground
+home.post("/", inputValidator, async (req, res) => {
+  const datas = req.camp;
+  const newCamp = new campGround({
+    title: datas.title,
+    location: datas.location,
+  });
+  const saved = await newCamp.save();
+  res.redirect(`/${saved._id}`);
 });
-console.log("hello world!");
+//Show edit page
+home.get("/:id/edit",async (req,res)=>{
+  const id = req.params.id;
+  const datas = await campGround.findById(id);
+  res.render('campground/editPage',{datas});
+})
+
+//use method overriding
+home.use(methodOverride('_method'));
+
+//Edit Camp ground
+home.patch("/:id/edit",inputValidator,async (req,res)=>{
+    const datas = req.body;
+    const id  = req.params.id;
+    const {title,location} = datas;
+    const updated = await campGround.findByIdAndUpdate(id,{$set:{title:title,location:location}},{returnDocument:"after"});
+    res.redirect(`/${id}`);
+    console.log(updated)
+})
+
+//middlewares
+
+async function inputValidator(req, res, next) {
+  const datas = req.body;
+  const { title, location } = datas;
+  if (!title || !location) {
+    return res.status(400).json({ message: "Null input" });
+  } else {
+    req.camp = datas;
+    next();
+  }
+}
 module.exports = home;
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 //server dependancies
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const path = require('path');
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views'));
-app.use(express.urlencoded({extended:true}));
+const mongoose = require("mongoose");
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 //connection string
-mongoose.connect(process.env.MONGODB_URI).then(console.log('Connected')).catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(console.log("Connected"))
+  .catch((err) => console.log(err));
 
-const home = require('./routes/home')
-app.use("/",home);
-app.listen(3000)
+const home = require("./routes/home");
+app.use("/", home);
+app.listen(3000);
+
