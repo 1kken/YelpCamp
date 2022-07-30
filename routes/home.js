d9ce9fd05f6fd2e56ba8c6bf41524b96d9caab7b
1iken <tavorearljohn@gmail.com>
2022-07-30 22:25:05 +0800

    added boiler plates

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,10 @@ const express = require("express");
 const { findById } = require("../models/campground");
 const home = express.Router();
 const campGround = require("../models/campground");
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
+
+//use method overriding
+home.use(methodOverride("_method"));
 
 //show all campGrounds
 home.get("/", async (req, res) => {
@@ -34,27 +37,37 @@ home.post("/", inputValidator, async (req, res) => {
   res.redirect(`/${saved._id}`);
 });
 //Show edit page
-home.get("/:id/edit",async (req,res)=>{
+home.get("/:id/edit", async (req, res) => {
   const id = req.params.id;
   const datas = await campGround.findById(id);
-  res.render('campground/editPage',{datas});
-})
-
-//use method overriding
-home.use(methodOverride('_method'));
+  res.render("campground/editPage", { datas });
+});
 
 //Edit Camp ground
-home.patch("/:id/edit",inputValidator,async (req,res)=>{
-    const datas = req.body;
-    const id  = req.params.id;
-    const {title,location} = datas;
-    const updated = await campGround.findByIdAndUpdate(id,{$set:{title:title,location:location}},{returnDocument:"after"});
-    res.redirect(`/${id}`);
-    console.log(updated)
-})
+home.put("/:id", inputValidator, async (req, res) => {
+  const datas = req.body;
+  const id = req.params.id;
+  const { title, location } = datas;
+  try {
+    await campGround.findByIdAndUpdate(
+      id,
+      { $set: { title: title, location: location } },
+      { returnDocument: "after" }
+    );
+  } catch (error) {
+    console.log("something went bad");
+  }
+  res.redirect(`/${id}`);
+});
+
+//Delete Camp Ground
+home.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const deleted = await campGround.findByIdAndDelete(id);
+  res.redirect("/");
+});
 
 //middlewares
-
 async function inputValidator(req, res, next) {
   const datas = req.body;
   const { title, location } = datas;
@@ -65,4 +78,5 @@ async function inputValidator(req, res, next) {
     next();
   }
 }
+
 module.exports = home;
--- server.js
+++ server.js
@@ -3,11 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-
+app.engine('ejs', ejsMate);
 //connection string
 mongoose
   .connect(process.env.MONGODB_URI)
