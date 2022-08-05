2c9cf76aa549c4ba915d2f0e2e6e97843b9f7132
1iken <tavorearljohn@gmail.com>
2022-08-05 16:18:46 +0800

    added utilities

--- routes/campground.js
+++ routes/campground.js
@@ -4,15 +4,17 @@ const { findById } = require("../models/campground");
 const home = express.Router();
 const campGround = require("../models/campground");
 const methodOverride = require("method-override");
+const catchAsync = require('../Utilities/CatchAsync');
+const appError = require('../Utilities/ExpressError');
 
 //use method overriding
 home.use(methodOverride("_method"));
 
 //show all campGrounds
-home.get("/", async (req, res) => {
+home.get("/", catchAsync(async (req, res) => {
   const datas = await campGround.find({});
   res.render("campground/index", { datas });
-});
+}));
 
 //Show form page
 home.get("/new", (req, res) => {
@@ -20,14 +22,14 @@ home.get("/new", (req, res) => {
 });
 
 //show individual campground
-home.get("/:id", async (req, res) => {
+home.get("/:id", catchAsync(async (req, res) => {
   const id = req.params.id;
   const datas = await campGround.findById(id);
   res.render("campground/show", { datas });
-});
+}));
 
 //Add new Camp ground
-home.post("/", inputValidator, async (req, res) => {
+home.post("/", inputValidator, catchAsync(async (req, res) => {
   const datas = req.camp;
   const newCamp = new campGround({
     title: datas.title,
@@ -38,16 +40,17 @@ home.post("/", inputValidator, async (req, res) => {
   });
   const saved = await newCamp.save();
   res.redirect(`campground/${saved._id}`);
-});
+}));
+
 //Show edit page
-home.get("/:id/edit", async (req, res) => {
+home.get("/:id/edit", catchAsync(async (req, res) => {
   const id = req.params.id;
   const datas = await campGround.findById(id);
   res.render("campground/editPage", { datas });
-});
+}));
 
 //Edit Camp ground
-home.put("/:id", inputValidator, async (req, res) => {
+home.put("/:id", inputValidator, catchAsync(async (req, res) => {
   const datas = req.body;
   const id = req.params.id;
   const { title, location } = datas;
@@ -61,14 +64,14 @@ home.put("/:id", inputValidator, async (req, res) => {
     console.log("something went bad");
   }
   res.redirect(`/campground/${id}`);
-});
+}));
 
 //Delete Camp Ground
-home.delete("/:id", async (req, res) => {
+home.delete("/:id", catchAsync(async (req, res) => {
   const id = req.params.id;
   const deleted = await campGround.findByIdAndDelete(id);
   res.redirect("/campground");
-});
+}));
 
 //middlewares
 async function inputValidator(req, res, next) {
